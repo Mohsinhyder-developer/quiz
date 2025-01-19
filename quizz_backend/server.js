00b545e21d8e9184c4101307d87280db2b75a8f5
Mohsin-Hyder <tm296895@gmail.com>
2025-01-19 22:18:20 +0500

    first commit

--- quizz_backend/server.js
+++ quizz_backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/quiz-app', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://davidthomas93452:mohsinhyder@cluster0.be5l5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
