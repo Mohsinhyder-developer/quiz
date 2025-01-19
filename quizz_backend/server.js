7e315be0f68676aab3e58791b219cbfe0f08b30f
Mohsin-Hyder <tm296895@gmail.com>
2025-01-19 22:25:22 +0500

    changesdd

--- quizz_backend/server.js
+++ quizz_backend/server.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req,res) =>{
+  res.send("server is running.")
+})
+
 mongoose.connect('mongodb+srv://davidthomas93452:mohsinhyder@cluster0.be5l5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
